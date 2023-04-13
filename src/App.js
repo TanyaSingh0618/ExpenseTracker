3460bd212fd73fbff915793846965de1defa2039
Admin <admin@Tanyas-MacBook-Air.local>
2023-04-13 14:36:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -8,21 +8,25 @@ function App() {
   // const currentDate = date.format('LL');
   const expenseItems = [
     {
+      id: 'e1',
       title: 'tv',
       amount: '$200',
       date: new Date(2023, 0, 22)
     },
     {
+      id: 'e2',
       title: 'soap',
       amount: '$50',
       date: new Date()
     },
     {
+      id: 'e3',
       title: 'car Insurance',
       amount: '$260',
       date: new Date()
     },
     {
+      id: 'e4',
       title: 'rent',
       amount: '$400',
       date: new Date()
--- src/Component/Expenses/NewExpense/ExpenseForm.jsx
+++ src/Component/Expenses/NewExpense/ExpenseForm.jsx
@@ -1,21 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 function ExpenseForm() {
+    const [Information, setInformation] = useState({
+        entertitle: '',
+        enteramount: '',
+        enterdate: ''
+    })
+    const TitleChangeHandler = (e) => {
+        setInformation((prevState) => {
+            return { ...prevState, entertitle: e.target.value }
+        });
+    }
+    const AmountChangeHandler = (e) => {
+        setInformation((prevState) => {
+            return { ...prevState, enteramount: e.target.value }
+        });
+        // setInformation({ ...Information, amount: e.target.value });
+    }
+    const DateChangeHandler = (e) => {
+        setInformation((prevState) => {
+            return { ...prevState, enterdate: e.target.value }
+        });
+        // setInformation({ ...Information, enterdate: e.target.value });
+    }
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const expenseData = {
+            title: Information.entertitle,
+            amount: Information.enteramount,
+            date: new Date(Information.enterdate),
+        }
+        console.log(expenseData);
+        setInformation({
+            entertitle: '',
+            enteramount: '',
+            enterdate: ''
+        })
+    }
+
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new_expense__controls'>
                 <div className='new_expense__control'>
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' value={Information.entertitle} onChange={TitleChangeHandler} />
                 </div>
                 <div className='new_expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' value={Information.enteramount} min='0.01' step='0.01' onChange={AmountChangeHandler} />
                 </div>
                 <div className='new_expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' max='2025-12-31' />
+                    <input type='date' value={Information.enterdate} min='2019-01-01' max='2025-12-31' onChange={DateChangeHandler} />
                 </div>
 
             </div>
